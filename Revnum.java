ff5b7b865cc8a086e1f8e5ea8ca80cae9cf6267c
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-05 09:25:46 +0530

    Add Revnum.java to reverse an integer

---
+++ Revnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Revnum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+            
+        }
+        System.out.print(rev);
+    }
+}
