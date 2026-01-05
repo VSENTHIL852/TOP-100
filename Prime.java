65fb6b8b3aad6342fcb963bd3e1dfa5e9bfca14f
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-05 09:50:06 +0530

    Add prime number checking functionality
    Implement a program to check if a number is prime.

---
+++ Prime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Prime{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<=1){
+            System.out.println("Not a prime");
+            return;
+        }
+        for(int i=2;i<=n/2;i++){
+            if(n%i == 0){
+                System.out.println("not a prime");
+                return;
+            }
+        }
+        System.out.println("Prime");
+        
+    }
+}
