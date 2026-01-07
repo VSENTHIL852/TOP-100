56e565b176aa86ae31dd66e030ebce58dd3b6359
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-07 20:29:58 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -3,7 +3,7 @@ public class Armstrong{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int sum=0;
+        int sum=0; 
         while(n>0){
             int d=n%10;
             sum=sum+d*d*d;
